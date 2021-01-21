1016e1f363f1dc7975866fbdd28dd349293fe41d
Martin <martin.kotkamae@khk.ee>
2021-01-21 13:39:43 +0200

    Loodud ja testitud ühendus andmebaasiga

--- app/config/constants.php
+++ app/config/constants.php
@@ -5,3 +5,10 @@ define("PROJECT_NAME","pvk_vs20");
 define("APPROOT", dirname(dirname(__FILE__)));
 // URL root
 define("URLROOT", "http://".$_SERVER["HTTP_HOST"]."/".PROJECT_NAME);
+
+
+// db params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "pvk_vs20");
