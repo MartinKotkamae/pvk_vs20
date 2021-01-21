7c7d206adfa48e6eb5a341cdc8e8c490d70e7a95
Martin <martin.kotkamae@khk.ee>
2021-01-21 14:17:38 +0200

    Koostatud ja testitud päringu ettevalmistus, väärtustega täitmine ja käivitamine

--- app/libs/Database.php
+++ app/libs/Database.php
@@ -10,21 +10,49 @@ class Database
 
     private $dbh;
     private $error;
+    private $stmt;
     /**
      * Database constructor
      */
     public function __construct()
     {
-        $dsn = "mysql:host=".$this->host.";dbname".$this->dbname;
-        $option = array(
+        $dsn = 'mysql:host='.$this->host.';dbname='.$this->dbname;
+        $options = array(
             PDO::ATTR_PERSISTENT => true,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         );
         try {
-            $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
         } catch (PDOException $e){
             $this->error = $e->getMessage();
-            echo $this->error."<br>";
+            echo $this->error.'<br>';
         }
+    }
+
+    public function query($sql){
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    public function bind($param, $value, $type=null){
+        if(is_null($type)){
+            switch (true){
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindParam($param, $value, $type);
+    }
+
+    public function execute(){
+        return $this->stmt->execute();
     }
 }
